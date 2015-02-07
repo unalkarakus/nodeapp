abd190afa7ce7adfe7e45bee95ef6331ccb3c949
unalkarakus <UnalK@Mac-Of-MacBook.local>
2015-02-07 19:22:43 +0200

    hello

--- hello-world-server.js
+++ hello-world-server.js
@@ -3,4 +3,4 @@ http.createServer(function handler(req, res) {
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(' { text : Hello World }');
 }).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at 127.0.0.1 on Port:1337');
